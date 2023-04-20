d72f64fa4d8c81432434ba2f67f352857feee94f
Nikita Ignatov <alamacus@gmail.com>
2023-04-20 11:15:04 +0200

    Nato alphabet project

---
+++ nato-alphabet/main.py
@@ -0,0 +1,8 @@
+import pandas
+
+nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
+nato_dict = {row.letter: row.code for (index, row) in nato_df.iterrows()}
+
+user_input = input("Enter a word:\n").upper()
+result_list = [nato_dict[letter] for letter in user_input]
+print(result_list)
