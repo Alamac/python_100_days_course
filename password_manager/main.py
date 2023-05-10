dcfa80fc4bd9f8fd3e1a7d72479f070bc5a3d7c4
Nikita Ignatov <alamacus@gmail.com>
2023-05-10 19:28:47 +0200

    Password Generator project

---
+++ password_manager/main.py
@@ -0,0 +1,81 @@
+from tkinter import Canvas, Button, Label, Tk, PhotoImage, Entry, END, messagebox
+from pass_gen import generate_password
+import pyperclip
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+def process_password():
+    password = generate_password()
+    pass_entry.insert(0, password)
+    pyperclip.copy(password)
+
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
+def save_pass():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = pass_entry.get()
+
+    if len(website) > 0 and len(password) > 0:
+        is_ok = messagebox.askokcancel(title=website,
+                                       message=f"There are the details entered: \nEmail: {email}\nPassword: {password}"
+                                               f"\nIs it ok to safe?")
+        if is_ok:
+            row = f"{website} | {email} | {password}\n"
+            with open("data.txt", "a") as file:
+                file.write(row)
+            website_entry.delete(0, END)
+            pass_entry.delete(0, END)
+            website_entry.focus()
+    else:
+        messagebox.showwarning(message="Can't have website or password empty!")
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+
+window = Tk()
+window.title = "Password Manager"
+window.config(padx=50, pady=50)
+
+canvas = Canvas(width=200, height=200, highlightthickness=0)
+logo = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo)
+canvas.grid(row=0, column=1)
+
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+
+website_entry = Entry(width=35)
+website_entry.focus()
+website_entry.grid(row=1, column=1, columnspan=2)
+
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+
+email_entry = Entry(width=35)
+email_entry.insert(0, "alamacus@gmail.com")
+email_entry.grid(row=2, column=1, columnspan=2)
+
+pass_label = Label(text="Password:")
+pass_label.grid(row=3, column=0)
+
+pass_entry = Entry(width=21)
+pass_entry.grid(row=3, column=1, padx=(24, 0))
+
+generate_button = Button(text="Generate Password", command=process_password)
+generate_button.grid(row=3, column=2)
+
+add_button = Button(text="Add", width=36, command=save_pass)
+add_button.grid(row=4, column=1, columnspan=2)
+
+window.mainloop()
