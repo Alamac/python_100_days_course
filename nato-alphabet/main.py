e56a29f39baf2054182a5f9b7fbbafd8565feafa
Nikita Ignatov <alamacus@gmail.com>
2023-05-11 13:41:01 +0200

    Update Nato alphabet with error handling.

--- nato-alphabet/main.py
+++ nato-alphabet/main.py
@@ -2,7 +2,13 @@ import pandas
 
 nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in nato_df.iterrows()}
-
-user_input = input("Enter a word:\n").upper()
-result_list = [nato_dict[letter] for letter in user_input]
-print(result_list)
+success = False
+while not success:
+    user_input = input("Enter a word:\n").upper()
+    try:
+        result_list = [nato_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+    else:
+        success = True
+        print(result_list)
