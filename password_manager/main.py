82c41ac3b37e0734b9bc578923c54345cc8b6899
Nikita Ignatov <alamacus@gmail.com>
2023-05-11 14:09:16 +0200

    Refactored Pass Manager to store data in json. Added Search button functionality.

--- password_manager/main.py
+++ password_manager/main.py
@@ -1,9 +1,9 @@
 from tkinter import Canvas, Button, Label, Tk, PhotoImage, Entry, END, messagebox
 from pass_gen import generate_password
 import pyperclip
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
            'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
            'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -18,31 +18,55 @@ def process_password():
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-
-
 def save_pass():
     website = website_entry.get()
     email = email_entry.get()
     password = pass_entry.get()
 
-    if len(website) > 0 and len(password) > 0:
-        is_ok = messagebox.askokcancel(title=website,
-                                       message=f"There are the details entered: \nEmail: {email}\nPassword: {password}"
-                                               f"\nIs it ok to safe?")
-        if is_ok:
-            row = f"{website} | {email} | {password}\n"
-            with open("data.txt", "a") as file:
-                file.write(row)
-            website_entry.delete(0, END)
-            pass_entry.delete(0, END)
-            website_entry.focus()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
+
+    if len(website) == 0 or len(password) == 0 or len(email) == 0:
+        messagebox.askokcancel(title="Oops",
+                               message="Please make sure you haven't left any fields empty.")
     else:
-        messagebox.showwarning(message="Can't have website or password empty!")
+        try:
+            with open("data.json", "r") as file:
+                data = json.load(file)
+                data.update(new_data)
+        except FileNotFoundError:
+            with open("data.json", 'w') as file:
+                json.dump(new_data, file, indent=4)
+        else:
+            with open("data.json", "w") as file:
+                json.dump(data, file, indent=4)
+        website_entry.delete(0, END)
+        pass_entry.delete(0, END)
+        website_entry.focus()
+
+
+# ---------------------------- Search password ------------------------------- #
+def search_pass(website):
+    try:
+        with open("data.json", "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showwarning(message="No data file found!")
+        return
+    try:
+        website_data = data[website]
+    except KeyError:
+        messagebox.showwarning(title=website, message=f"No password found for {website}")
+    else:
+        messagebox.showinfo(title=website,
+                            message=f"Email: {website_data['email']}\nPassword: {website_data['password']}")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
-
-
 window = Tk()
 window.title = "Password Manager"
 window.config(padx=50, pady=50)
@@ -55,9 +79,9 @@ canvas.grid(row=0, column=1)
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
 
-website_entry = Entry(width=35)
+website_entry = Entry(width=25)
 website_entry.focus()
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.grid(row=1, column=1, padx=(50, 0))
 
 email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
@@ -77,5 +101,8 @@ generate_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=36, command=save_pass)
 add_button.grid(row=4, column=1, columnspan=2)
+
+search_button = Button(text="Search", command=lambda: search_pass(website_entry.get()))
+search_button.grid(row=1, column=2)
 
 window.mainloop()
